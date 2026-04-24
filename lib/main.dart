75b0cafe0eda22e6b72af12e782e8d7c737ad891
QasimQamar <syedqasimqamarabbasjaffri@gmail.com>
2026-04-24 14:30:11 +0500

    Chore: Initialize Firebase Core backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart'; // Importing your design system
 import 'features/auth/splash_screen.dart'; 
 import 'features/auth/login_screen.dart';
 import 'features/auth/registration_screen.dart';
-void main() {
+
+
+void main() async {
+  // This line is required when using async operations before runApp()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // This is the magic line that boots up your connection to Google!
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const SmartStudyApp());
-}
-
-class SmartStudyApp extends StatelessWidget {
-  const SmartStudyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SmartStudy',
-      debugShowCheckedModeBanner: false,
-      // Here is where we inject your custom design system!
-      theme: AppTheme.lightTheme, 
-      // We will replace this placeholder with your actual Splash Screen in the next step
-      home: const LoginScreen(),
-    );
-  }
 }
